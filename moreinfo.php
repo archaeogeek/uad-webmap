ae0c302efba71287643870b71f4665c1e62f8bf5
archaeogeek <jo.k.cook@gmail.com>
2016-03-23 18:44:14 +0000

    First attempt at the json stuff in php

--- moreinfo.php
+++ moreinfo.php
@@ -1,3 +1,20 @@
 <?php
-echo 'Ref No = ' . htmlspecialchars( $_GET["refno"]) . ' and Table = ' . htmlspecialchars( $_GET["table"])
+$refno = htmlspecialchars( $_GET["refno"]);
+echo $refno;
+$table = htmlspecialchars( $_GET["table"]);
+echo $table;
+$URL = "http://lancasteruad.oxfordarchaeology.com/rest/index.php/" . $table . "/UADReferenceNumber/" . $refno;
+echo $URL;
+$json = file_get_contents($url);
+$jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode($json, TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
+
+foreach ($jsonIterator as $key => $val) {
+    if(is_array($val)) {
+        echo "$key:\n";
+    } else {
+        echo "$key => $val\n";
+    }
+}
 ?>
